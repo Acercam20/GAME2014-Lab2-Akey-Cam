554be16603f8da075fb34bf7dae3099e6d6b7c16
Acercam20 <cameronakey@hotmail.ca>
2020-10-05 10:28:06 -0400

    Removed assets re-added

---
+++ Assets/_Scripts/EndButtonBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndButtonBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnEndButtonPressed()
+    {
+        Debug.Log("EndButton Pressed");
+        SceneManager.LoadScene("Start");
+    }
+}
